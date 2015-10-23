d09d49243bbfe74ae7dfc3e94005ec8f19d0a72b
Greg Poole <gregory.poole@sydney.edu.au>
2015-10-23 21:47:31 +1100

    Add auto-scrolling when bubbles are added

--- src/views/listen.js
+++ src/views/listen.js
@@ -2,6 +2,7 @@ import { inject, bindable, computedFrom } from 'aurelia-framework';
 import { TranscriptStore } from 'lib/transcript-store';
 import { SpeechService } from 'lib/speech-service';
 import { EventAggregator } from 'aurelia-event-aggregator';
+import $ from 'jquery';
 
 @inject(EventAggregator, TranscriptStore, SpeechService)
 export class Listen {
@@ -21,15 +22,28 @@ export class Listen {
 
 		this.disposeHandlers.push(this.eventAggregator.subscribe('transcript:added', (transcript) => {
 			this.transcripts.push(transcript);
+			// FIXME: there's got to be a hook for the render update and a way to get the view's DOM
+			// element, but if there is I can't find it...
+			setTimeout(() => {
+				let lastBubble = $('.se-bubble:last');
+				lastBubble.addClass('se-bubble-added');
+				let bubbleBottom = lastBubble.offset().top + lastBubble.outerHeight();
+
+				if(bubbleBottom > $(window).height()) {
+					$('body').animate({
+						scrollTop: lastBubble.offset().top
+					});
+				}
+			}, 0);
 		}));
 
 		this.disposeHandlers.push(this.eventAggregator.subscribe('speech:stopping', () => {
 			console.log("Speech recognition is stopping.");
-			this.running = false
+			this.running = false;
 		}));
 		this.disposeHandlers.push(this.eventAggregator.subscribe('speech:starting', () => {
 			console.log("Speech recognition is starting.");
-			this.running = true
+			this.running = true;
 		}));
 		this.speechService.start();
 	}
